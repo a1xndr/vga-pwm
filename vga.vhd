64b1db7a1d07e3466f3c524f389b0f8f43c8064c
Alexander Oleinik <alxndr@solo.vrmnet>
2016-04-20 17:04:34 -0400

    it works

--- vga.vhd
+++ vga.vhd
@@ -7,8 +7,7 @@ use ieee.std_logic_unsigned.all;
 
 entity VGAdrive is
   port( clock            : in std_logic;  -- 25.175 Mhz clock
-        red, green, blue : in std_logic;  -- input values for RGB signals
-        row, column : out std_logic_vector(9 downto 0); -- for current pixel
+         -- for current pixel
         Rout, Gout, Bout, H, V : out std_logic); -- VGA drive signals
 
 end;
@@ -28,11 +27,12 @@ architecture behaviour1 of VGAdrive is
   constant T : natural := P + Q + R + S;  -- one vertical sync cycle: 
   
   signal clockdiv : std_logic;
+  signal row, column : std_logic_vector(9 downto 0);
+  signal red, green, blue : std_logic;
+
+  
 begin
 
-  Rout <= red;
-  Gout <= green;
-  Bout <= blue;
 
 	process(clock)
 		begin
@@ -72,12 +72,36 @@ begin
         V <= '1';
       end if;
 	end if;
-	 if  row < 100 and column < 350  then
-      red <= '1';
+	 if column < 80  then
+	 	red <= '1';
 		green <= '1';
 		blue <= '1';
-    else
-      red <= '0';
+	 elsif column < 160  then
+	 	red <= '1';
+		green <= '1';
+		blue <= '0';
+	 elsif column < 240  then
+	 	red <= '0';
+		green <= '1';
+		blue <= '1';
+	 elsif column < 320  then
+	 	red <= '0';
+		green <= '1';
+		blue <= '0';
+	 elsif column < 400  then
+	 	red <= '1';
+		green <= '0';
+		blue <= '1';
+	 elsif column < 480  then
+	 	red <= '1';
+		green <= '0';
+		blue <= '0';
+	 elsif column < 560  then
+	 	red <= '0';
+		green <= '0';
+		blue <= '1';
+	 else 
+	 	red <= '0';
 		green <= '0';
 		blue <= '0';
     end if;
@@ -87,58 +111,11 @@ begin
     row <= vertical;
     column <= horizontal;
 
+	Rout <= red;
+	Gout <= green;
+	Bout <= blue;
 
   end process;
 
 end architecture;
 
-
--- RGB VGA test pattern  Rob Chapman  Mar 9, 1998
-
- -- This file uses the VGA driver and creates 3 squares on the screen which
- -- show all the available colors from mixing red, green and blue
-
-Library IEEE;
-use IEEE.STD_Logic_1164.all;
-use ieee.std_logic_arith.all;
-use ieee.std_logic_unsigned.all;
-
-entity vgatest is
-  port(clock         : in std_logic;
-       R, G, B, H, V : out std_logic);
-end entity;
-
-architecture test of vgatest is
-
-  component vgadrive is
-    port( clock          : in std_logic;  -- 25.175 Mhz clock
-        red, green, blue : in std_logic;  -- input values for RGB signals
-        row, column      : out std_logic_vector(9 downto 0); -- for current pixel
-        Rout, Gout, Bout, H, V : out std_logic); -- VGA drive signals
-  end component;
-  
-  signal row, column : std_logic_vector(9 downto 0);
-  signal red, green, blue : std_logic;
-
-begin
-
-  -- for debugging: to view the bit order
-  VGA : component vgadrive
-    port map ( clock => clock, red => red, green => green, blue => blue,
-               row => row, column => column,
-               Rout => R, Gout => G, Bout => B, H => H, V => V);
-
-  RGB : process(clock, row, column)
-  begin
-    if  row < 100 and column < 350  then
-      red <= '1';
-		green <= '1';
-		blue <= '1';
-    else
-      red <= '0';
-		green <= '0';
-		blue <= '0';
-    end if;
-    end process;
-
-end architecture;
